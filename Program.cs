5e31a5b9c98522c3beee701826801438c4b5a5b0
Lee-Denham1988 <Lee-Denham1988@users.noreply.github.com>
2022-05-02 16:05:09 +0100

    Insert initial functionality
    Getting initial file-rename functionality working.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+static void main ()
+{
+
+    try
+    {
+
+        while (true)
+        {
+            string FileSource = @"\\nas2.lan\ConfigBackups\backup_.conf";
+
+            bool FileExists = File.Exists(FileSource);
+
+            if (FileExists)
+            {
+                string DateTimeLog = DateTime.Now.ToString("dd_MM_yyyy_HH_mm");
+                string FileDest = @"\\nas2.lan\ConfigBackups\" + DateTimeLog + "_backup.conf";
+
+                File.Move(FileSource, FileDest); //Move files to destination.
+                
+            }
+        }
+
+    }
+    catch (IOException ex)
+    {
+        Console.WriteLine(ex); 
+    }
+
+
+}
+
+main();
+
