c404fbe8539946d7a1512ae2221bcb955e65b1b6
Lee-Denham1988 <Lee-Denham1988@users.noreply.github.com>
2023-10-19 14:40:50 +0100

    Added file management by date
    Adding functionality to manage files into subfolders with day month to facilitate retention management.

--- Program.cs
+++ Program.cs
@@ -5,20 +5,34 @@ static void main ()
 
     try
     {
+        Console.WriteLine("Fortigate Backup Service Starting...");
 
         while (true)
         {
-            string FileSource = @"\\nas2.lan\ConfigBackups\backup_.conf";
+            string RootFilePath = @"\\NASBACKUP\Backup\InfrastructureBackups\FortiGates\";
+            string TempRootFilePath = @"\\nasbackup\backup\InfrastructureBackups\FortiGateBackupTool\TempStore\";
+            //string RootFilePath = @"D:\test\"; //Local testing path
+
+            String Todaysdate = DateTime.Now.ToString("dd-MMMM-yyyy");
+            
+            if (!Directory.Exists(RootFilePath + Todaysdate))
+            {
+                Directory.CreateDirectory(RootFilePath + Todaysdate);
+            }
+            //Console.WriteLine(Todaysdate); //Printing foldername to test.
+
+            string FileSource = TempRootFilePath + "\\backup_.conf";
 
             bool FileExists = File.Exists(FileSource);
 
             if (FileExists)
             {
                 string DateTimeLog = DateTime.Now.ToString("dd_MM_yyyy_HH_mm");
-                string FileDest = @"\\nas2.lan\ConfigBackups\" + DateTimeLog + "_backup.conf";
+                string FileDest = RootFilePath + "\\" + Todaysdate + "\\"+ DateTimeLog + "_backup.conf";
 
-                File.Move(FileSource, FileDest); //Move files to destination.
-                
+               File.Move(FileSource, FileDest); //Move files to destination.
+                Console.WriteLine("Moving files to: " + FileDest);
+
             }
         }
 
